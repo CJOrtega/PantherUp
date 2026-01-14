eacb49e33f3210277f04a2950cb76ffd2ffdc8f9
COrtega <cristopherortegaimportant@gmail.com>
2026-01-14 14:08:07 -0500

    Start Spring Boot Project

---
+++ src/test/java/com/panthers/pantherup/PantherUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panthers.pantherup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantherUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
