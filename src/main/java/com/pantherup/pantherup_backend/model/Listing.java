fa9c1a53a2e43e0abb033eb15ff3d53b2ebb4c7d
Christopher Cifuentes <cifuenteschristopher55@gmail.com>
2026-02-03 10:39:31 -0500

    Added lombok features to the listing class and worked on local testing with H2

--- src/main/java/com/pantherup/pantherup_backend/model/Listing.java
+++ src/main/java/com/pantherup/pantherup_backend/model/Listing.java
@@ -1,24 +1,24 @@
 package com.pantherup.pantherup_backend.model;
 
 import com.pantherup.pantherup_backend.config.Status;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 
+@Data
 @Entity
+@Table(name = "Listings")
 public class Listing {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long listingId;
-    @JoinColumn
     private Long userId;
     Integer price;
     String listingTitle;
     String listingDescription;
     String listingImagesId;
+    @Enumerated(EnumType.STRING)
     Status status;
     LocalDate listingDate;
 
